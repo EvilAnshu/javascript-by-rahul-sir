9e0f38eee7e22c12c0f404b886495938eda1e384
Anshu Verma <anshuverma555.av@gmail.com>
2022-12-23 12:18:08 +0530

    Start with Module

---
+++ m1.js
@@ -0,0 +1,18 @@
+export var firstname = "RAM";
+export var lastname = "Kumar";
+
+export function fun1()
+{
+    alert("Hello I am From fun1 function present in m1 module");
+}
+export function fun2()
+{
+    alert("Hello I am From fun2 function present in m1 module");
+}
+export class test
+{
+    constructor()
+    {
+        alert("Hello i am from Constructor method present in text class");
+    }
+}
